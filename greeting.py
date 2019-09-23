f4d1f104a6d926b0cfb0625f2c38791379f9db96
Mattie Zilaie <your_name@domain.comAccount>
2019-09-23 16:07:09 -0700

    greeting with name input

---
+++ greeting.py
@@ -0,0 +1,7 @@
+# Author: Mahtab Zilaie
+# Date: 9/23/2019
+# Description: Asks the user for their name and then
+#              prints out
+
+greet_with_name = input("Please enter your name: ")
+print("Hello " + greet_with_name+ "!")
